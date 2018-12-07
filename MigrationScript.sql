e24f9bada1cfe2a5154a0b3b8f6a35ff8bd2dc89
Yuresh <yureshcs@gmail.com>
2018-12-07 13:09:05 +0530

    new Migration script

--- MigrationScript.sql
+++ MigrationScript.sql
@@ -1,1 +1,15 @@
-insert into SLTTestDatabase values (2, 'yuresh' , 'Akuressa' )
+CREATE TABLE TestSQL1 (
+    PersonID int,
+    Name varchar(255),
+    City varchar(255) 
+)
+
+insert into TestSQL1 values (1,'yuresh','Matara')
+
+insert into TestSQL1 values (2,'yuresh','Akuressa')
+
+DROP TABLE TestSQL
+
+ALTER TABLE TestSQL1 DROP COLUMN City
+
+update TestSQL1 SET Name='Isuri' where PersonID=2
